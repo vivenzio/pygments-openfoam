4fd85bb6fd566d292a5634ee89d86578c73fcc25
Kasper Linnestad <kasper1301@users.noreply.github.com>
2014-08-04 14:24:17 +0200

    Added more class names

--- pygments_openfoam/__init__.py
+++ pygments_openfoam/__init__.py
@@ -17,11 +17,28 @@ class OpenFOAMLexer(CppLexer):
     aliases = ['openfoam']
     filenames = ['*.C', '*.H'] # just to have one if you want to use
 
-    EXTRA_CLASSNAMES = ['volVectorField', 'volScalarField']
+    EXTRA_CLASSNAMES = ['volVectorField',
+                        'volScalarField',
+                        'twoPhaseSystem',
+                        'phaseModel',
+                        'surfaceScalarField',
+                        'dimensionedScalar',
+                        'rhoReactionThermo',
+                        'basicMultiComponentMixture',
+                        'PtrList',
+                        'word',
+                        'IOobject',
+                        'Info',
+                        'label',
+                        'scalar',
+                        'fvScalarMatrix']
+    EXTRA_KEYWORDS = ['forAll']
 
     def get_tokens_unprocessed(self, text):
         for index, token, value in CppLexer.get_tokens_unprocessed(self, text):
             if token is Name and value in self.EXTRA_CLASSNAMES:
                 yield index, Name.Class, value
+            else if token is Name and value in self.EXTRA_KEYWORDS:
+                yield index, Keyword, value
             else:
                 yield index, token, value
